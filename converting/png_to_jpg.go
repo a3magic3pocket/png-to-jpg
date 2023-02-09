d6e00a1d1418f1ee813a55aecd5ed97924f1e450
a3magic3pocket <a3magic3pocket@gmail.com>
2023-02-09 21:34:21 +0900

    Change logic that convert png to jpeg.
    Add resizing.
    Add jpeg compression logic.

--- converting/png_to_jpg.go
+++ converting/png_to_jpg.go
@@ -1,14 +1,17 @@
 package converting
 
 import (
-	"bufio"
 	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/jpeg"
+	_ "image/png"
 	"log"
 	"os"
 	"png_to_jpg/utils"
 	"strings"
 
-	"github.com/sunshineplan/imgconv"
+	"github.com/disintegration/imaging"
 )
 
 func getPngPaths(path string) []string {
@@ -45,15 +48,14 @@ func PngToJpg(sourceDirPath string, resultDirPath string) {
 	fmt.Println("png files: ", pngPaths)
 
 	for _, pngPath := range pngPaths {
-		src, err := imgconv.Open(pngPath)
-		if err != nil {
-			log.Fatalf("failed to open image: %v", err)
-		}
+		maxWidth := 800
+		jpegQuality := 70
 
 		_, others := utils.GetExtenstion(pngPath)
+
+		// Create new File
 		newFileOthers := getNewFilePath(sourceDirPath, resultDirPath, others)
 		newFilePath := fmt.Sprintf("%s.jpg", newFileOthers)
-
 		newFile, err := os.OpenFile(
 			newFilePath,
 			os.O_CREATE|os.O_RDWR|os.O_TRUNC,
@@ -63,8 +65,35 @@ func PngToJpg(sourceDirPath string, resultDirPath string) {
 			log.Fatalf("failed to create newFile: %v", err)
 		}
 
-		w := bufio.NewWriter(newFile)
-		imgconv.Write(w, src, &imgconv.FormatOption{Format: imgconv.JPEG})
+		// Open origin image file
+		imageFile, err := os.Open(pngPath)
+		if err != nil {
+			log.Fatalf("failed to open image: %v", err)
+		}
+
+		// Decode origin image file
+		src, _, err := image.Decode(imageFile)
+		if err != nil {
+			log.Fatalf("failed to decode image: %v", err)
+		}
+		imageFile.Close()
+
+		// Resize image if origin image is too big
+		width := src.Bounds().Max.X
+		if width > maxWidth {
+			width = maxWidth
+		}
+		src = imaging.Resize(src, width, 0, imaging.Lanczos)
+
+		// Encode resized image to jpg:
+		jpegOptions := jpeg.Options{
+			Quality: jpegQuality,
+		}
+		err = jpeg.Encode(newFile, src, &jpegOptions)
+		if err != nil {
+			log.Fatalf("failed to ecode image to jpg: %v", err)
+		}
+
 		newFile.Close()
 
 		fmt.Println("png to jpg file: ", newFilePath)
